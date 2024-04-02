076f0497b2df2a4f0bbbf8eca41adc7eb887b9d9
voismager <grastasss@gmail.com>
2024-04-02 15:30:25 +0200

    Update pom and test

--- src/test/kotlin/happyshapes/GenerateExampleImage.kt
+++ src/test/kotlin/happyshapes/GenerateExampleImage.kt
@@ -5,5 +5,7 @@ import java.io.FileOutputStream
 
 fun main() {
     val g = HappyShapesGenerator()
-    g.writeAsSvg("Liza", "Liza12", FileOutputStream("out.svg"))
+    val mainComponent = "Walton"
+    val keyComponent = "MqTryK1S1ub6SWgASFUc7"
+    g.writeAsSvg(mainComponent, keyComponent, FileOutputStream("$mainComponent-$keyComponent.svg"))
 }
